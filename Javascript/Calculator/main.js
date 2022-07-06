da757e2ad030b874f77e14a1dbe8a920ecd80511
r1ks-iwnl <69760936+r1ks-iwnl@users.noreply.github.com>
2022-07-06 14:26:02 +0300

    do later

--- Javascript/Calculator/main.js
+++ Javascript/Calculator/main.js
@@ -1,1 +1,11 @@
+let buttons = document.getElementsByTagName("button");
+let display = document.getElementById("display");
+//buttons need an identifier
+for (var i = 0 ; i < buttons.length; i++) {
+	buttons[i].addEventListener("click", addNumber);
+}
 
+function addNumber(){
+	display = i++;
+	console.log(display);
+}
