823136ceb0aa2ee0e81835a4cc003483a275e5e5
r1ks-iwnl <69760936+r1ks-iwnl@users.noreply.github.com>
2022-07-12 15:40:55 +0300

    numbers actually display now

--- Javascript/Calculator/main.js
+++ Javascript/Calculator/main.js
@@ -3,9 +3,11 @@ let button = [];
 
 for (let i = 1; i <= 9; i++) {
 	button[i] = document.getElementById([i]);
-	button[i].addEventListener("click", addNumber(i), console.log(i)); //clicking unresponsive
+	button[i].addEventListener("click", function(){
+		addNumber(i);
+	});
 }
 
 function addNumber(number){
-	display.innerHTML = number;
+	display.innerHTML += number;
 }
