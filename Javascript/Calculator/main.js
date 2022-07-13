c1e49613f3ef6bade2a3c526f591d2bc9e27311f
r1ks-iwnl <69760936+r1ks-iwnl@users.noreply.github.com>
2022-07-13 15:26:57 +0300

    operators 1

--- Javascript/Calculator/main.js
+++ Javascript/Calculator/main.js
@@ -1,5 +1,10 @@
 let display = document.getElementById("display");
 let button = [];
+//Could be better
+document.getElementById("plus").addEventListener("click", function(){addNumber("+");});
+document.getElementById("minus").addEventListener("click", function(){addNumber("-");});
+document.getElementById("divide").addEventListener("click", function(){addNumber("/");});
+document.getElementById("multiply").addEventListener("click", function(){addNumber("*");});
 
 for (let i = 1; i <= 9; i++) {
 	button[i] = document.getElementById([i]);
